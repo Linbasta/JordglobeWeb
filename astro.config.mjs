2499e2587ee140da8541a4592a23dddacd816eea
fnarkas <olle.landin@gmail.com>
2025-02-16 13:41:12 +0100

    Add: a first commit

---
+++ astro.config.mjs
@@ -0,0 +1,72 @@
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import compress from "@playform/compress";
+import AutoImport from "astro-auto-import";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
+import netlify from "@astrojs/netlify";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://nova.cosmicthemes.com",
+	adapter: netlify({
+		imageCDN: false,
+	}),
+	redirects: {
+		"/admin": "/keystatic",
+	},
+	// i18n configuration must match src/config/translations.json.ts
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "fr"],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
+	markdown: {
+		shikiConfig: {
+			// Shiki Themes: https://shiki.style/themes
+			theme: "css-variables",
+			wrap: false,
+		},
+	},
+	// trailingSlash: "always",
+	integrations: [
+		// example auto import component into mdx files
+		AutoImport({
+			imports: [
+				// https://github.com/delucis/astro-auto-import
+				"@components/Admonition/Admonition.astro",
+			],
+		}),
+		mdx(),
+		react(),
+		icon(),
+		keystatic(),
+		sitemap(),
+		compress({
+			HTML: true,
+			JavaScript: true,
+			CSS: true,
+			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
+			SVG: false, // astro-icon handles this
+		}),
+	],
+
+	vite: {
+		plugins: [tailwindcss()],
+		// stop inlining short scripts to fix issues with ClientRouter: https://github.com/withastro/astro/issues/12804
+		build: {
+			assetsInlineLimit: 0,
+		},
+	},
+	experimental: {
+		svg: {
+			mode: "sprite",
+		},
+	},
+});
