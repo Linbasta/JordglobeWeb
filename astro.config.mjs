e61ae596add3d0833c0c53fdc005b0716b31d1a7
Olle Landin <olle@keypunch.se>
2025-02-25 12:40:06 +0100

    Bug: redircetion was going to the astro theme, not the jordglobe-homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://nova.cosmicthemes.com",
+	site: "https://jordglobe.com",
 	adapter: netlify({
 		imageCDN: false,
 	}),
